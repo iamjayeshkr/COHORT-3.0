d28bb3628a51126514c0a221672f7a7017d86efe
iamjayeshkr <bittuofficial44@gmail.com>
2026-08-13 23:30:12 +0530

    Typescript Completed

---
+++ Typescript/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let num = 10;
+num = 20;
+console.log(num);
+//# sourceMappingURL=index.js.map
---
+++ Typescript/index.ts
@@ -0,0 +1,6 @@
+let num: number = 10
+
+num  = 20
+console.log(num);
+
+
